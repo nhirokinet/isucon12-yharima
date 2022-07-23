fc128aa9aebe521f89b57dda02e6b222dcb04a79
Yuta Harima <yuu725@gmail.com>
2022-07-23 13:48:13 +0900

    migrate SQLite data to MySQL

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -16,6 +16,8 @@ mysql -u"$ISUCON_DB_USER" \
 		--port "$ISUCON_DB_PORT" \
 		"$ISUCON_DB_NAME" < init.sql
 
-# SQLiteのデータベースを初期化
-rm -f ../tenant_db/*.db
-cp -r ../../initial_data/*.db ../tenant_db/
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
+		--host "$ISUCON_DB_HOST" \
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < competition-and-player.sql
